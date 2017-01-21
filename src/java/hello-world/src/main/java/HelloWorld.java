b48f1a56969c16169702006fc6ecf6e562368bbb
Roberto Nogueira <enogrob@gmail.com>
2017-01-21 07:36:42 -0200

    first commit

---
+++ src/java/hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static String hello(String name) {
+       name = "Beto";
+       if (name == null) {
+    
+       }
+       else if () {
+    
+       }
+       if(name == null) {
+         System.out.println("Yes");
+       } else {
+
+       }
+        return null;
+    }
+}
